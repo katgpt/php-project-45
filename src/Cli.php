d3f07c12d7b1b88fc776f772d6bec90d4ea902e4
katgpt <katgpt2024@gmail.com>
2024-03-22 23:22:04 +0600

    step3end

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Src\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function welcome()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    return $name;
+}
